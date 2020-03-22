d7d622e30fbf82232dc0ed322a6fed056b08b79c
Tatyana Silchenko <taniasilchenko212@gmail.com>
2020-03-22 19:07:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,8 +4,38 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    if(expr.includes(' ')){
+        arr = expr.split(' ');
+    } else {
+        arr = expr.split('');
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == ' ' || arr[i] == ''){
+            arr.splice(i, 1);
+        }
+    }
+    let result = arr[0];
+    for(let i = 1; i < arr.length; i = i+2){
+        console.log(result)
+        if(arr[i] == '+'){
+            result += arr[i + 1];
+        }
+        if(arr[i] == '-'){
+            result -= arr[i + 1];
+        }
+        if(arr[i] == '*'){
+            result *= arr[i + 1];
+        }
+        if(arr[i] == '/'){
+            result /= arr[i + 1];
+            console.log(result)
+        }
+    }
+    return +result.toFixed(4);
 }
+
+
 
 module.exports = {
     expressionCalculator
